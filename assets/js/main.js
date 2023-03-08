bfd16752fccf9cfecdeea9564e5ae8861779d854
alessiaBer <alexdeyn@gmail.com>
2023-03-08 15:13:35 +0100

    [add] js steps

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,18 @@ il prezzo del biglietto è definito in base ai km (0.21 € al km)
 va applicato uno sconto del 20% per i minorenni
 va applicato uno sconto del 40% per gli over 65.
 L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). */
+
+// Chiedo al passeggero il numero di km da percorrere e lo assegno ad una variabile
+
+// Chiedo al passeggero la sua età e la assegno ad una variabile
+
+
+/* Calcolo il prezzo totale del biglietto */
+// Assegno il prezzo euro per km ad una variabile
+
+// Dichiaro la condizione dello sconto del 20% per i minorenni
+
+// Dichiaro la condizione dello sconto del 40% per gli over 65
+
+
+// Output del prezzo 
